bbd8d08b85378c47d69d01fdbecca1a0f7d65735
daramosn <daramosn@unal.edu.co>
2024-05-26 15:02:00 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <MoviesHeader></MoviesHeader>
+</template>
+
+<script lang="ts" setup>
+import MoviesHeader from './components/MoviesHeader.vue'
+</script>
---
+++ src/components/MoviesHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="movies-header">
+    <nav>
+      <h1>Movies app</h1>
+    </nav>
+    <div>{{ counter }}</div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const counter = ref(10)
+</script>
+
+<style>
+.movies-header {
+  margin: 32px max(32px, calc((100vw - var(--body)) / 2));
+}
+</style>
