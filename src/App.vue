013e7d5d10d1dc9cb64e18e4e19cd55f2aa000f7
daramosn <daramosn@unal.edu.co>
2024-05-28 10:23:29 -0500

    feat: add app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,77 @@
 <template>
-  <MoviesHeader></MoviesHeader>
+  <MoviesHeader />
+  <main>
+    <!-- Searchbox and filters-->
+    <section>
+      <input type="text" v-model="searchInput" @keypress.enter="searchMovie" required />
+      <button @click="searchMovie">Search</button>
+    </section>
+
+    <!-- Movies -->
+    <section class="movies-container">
+      <p v-if="isSearching">Searching...</p>
+      <p v-else-if="error">Sorry, there was an error.</p>
+      <ul v-else>
+        <li v-for="movie in movies" :key="movie.id">
+          <img :src="movie.poster_path" alt="movie" />
+          <div>
+            <small>{{ movie.release_date }}</small> <br />
+            <span>{{ movie.title }}</span>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script lang="ts" setup>
-import MoviesHeader from './components/MoviesHeader.vue'
+import MoviesHeader from '@/components/MoviesHeader.vue'
+import { useMovies } from './composables/useMovie'
+
+const { isSearching, movies, searchInput, searchMovie, error } = useMovies()
 </script>
+
+<style>
+main {
+  margin: 32px max(32px, calc((100vw - var(--body)) / 2));
+}
+.movies-container {
+  ul {
+    padding: 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(min(200px, 100%), 1fr));
+    gap: 32px;
+
+    li {
+      list-style: none;
+      animation: fade-in 0.5s forwards cubic-bezier(0.44, -0.08, 0.34, 1.17);
+    }
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 5/8;
+    object-fit: cover;
+    border-radius: 4px;
+    transition: all 0.2s ease-out;
+    &:hover {
+      scale: 1.03;
+    }
+  }
+
+  p {
+    font-size: 24px;
+  }
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-200);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
