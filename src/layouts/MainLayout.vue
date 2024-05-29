a01b4fe482881d6595af57c6e40bede9ca4915fb
daramosn <daramosn@unal.edu.co>
2024-05-28 21:55:57 -0500

    styles: add styles to layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,3 +8,9 @@
 <script lang="ts" setup>
 import MoviesHeader from '@/components/MoviesHeader.vue'
 </script>
+
+<style>
+main {
+  margin: 32px max(32px, calc((100vw - var(--body)) / 2));
+}
+</style>
